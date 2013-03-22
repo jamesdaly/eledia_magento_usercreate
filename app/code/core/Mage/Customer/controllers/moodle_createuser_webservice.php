9fb48981acd8863c7d3698d3c3e226dc403cc9c0
Benjamin Wolf <benjamin.wolf@eledia.de>
2013-03-22 17:29:29 +0100

    add checks and exceptions

--- app/code/core/Mage/Customer/controllers/moodle_createuser_webservice.php
+++ app/code/core/Mage/Customer/controllers/moodle_createuser_webservice.php
@@ -3,6 +3,8 @@
 //echo "in script\n<br />";
 //echo $email.$password.$firstname.$lastname;
 
+    if ($this->getRequest()->getPost('email') && $this->getRequest()->getPost('password') && !empty($customer->getFirstname()) && !empty($customer->getLastname())) {
+//echo "in if\n<br />";
         try {//build up soap client
             $client = new SoapClient(
                 NULL,
@@ -14,11 +16,12 @@
                     'trace' => 1
                 )
             );
-//        }  catch (exception $e){
-//            print_r($client->__getLastResponse());
-//        }
+        }  catch (exception $e){
+			$session->setCustomerFormData($this->getRequest()->getPost())
+                    ->addException($e, $this->__('Error while building up soap client.'));
+        }
 //print_r($client);
-//        echo "client build succsessful\n<br />";
+//echo "client build succsessful\n<br />";
 
         //build moodle user
         $param_users = array(new stdClass);
@@ -32,18 +35,15 @@
         $param_users[0]->lang = 'de';
         $param_users[0]->city = 'testlingen';
         $param_users[0]->country = 'DE';
-//        echo "try call\n<br />";
-        print_r($param_users);
-       
+//echo "try call\n<br />";
+//print_r($param_users);
+        try{
             $result = $client->core_user_create_users($param_users);
         }catch(exception $e){
-//            print_r($client->__getLastResponse());//hier Fehlermeldung
+//print_r($client->__getLastResponse());
             $session->setCustomerFormData($this->getRequest()->getPost())
                     ->addException($e, $this->__('Cannot save the customer to Moodle.'));
         }
-//        
-        //test if user submitted correct
-//    }
+    }
 //echo "leaving script\n<br />";
 //exit;
-
